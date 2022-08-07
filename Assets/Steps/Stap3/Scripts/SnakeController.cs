74737f215aec26651a87cc752f69d8f2ac6f280b
Roma Ag <93056140+Tiraniell@users.noreply.github.com>
2022-08-08 01:03:32 +0300

    some commit
    some commit

---
+++ Assets/Steps/Stap3/Scripts/SnakeController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    public List<Transform> Tails;
+    [Range(0,1)]
+    public float BonesDistanse;
+    public GameObject BonePrefabe;
+
+    [Range(0, 1)]
+    public float Speed;
+    
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        MoveSnake(_transform.position + _transform.forward * Speed);
+
+        float angel = Input.GetAxis("Horizontal") * 4;
+        _transform.Rotate(0, angel, 0);
+    }
+
+    private void MoveSnake(Vector3 newPosition)
+    {
+        float sqrDistance = BonesDistanse * BonesDistanse;
+        Vector3 previosPosition = _transform.position;
+
+        foreach(var bone in Tails)
+        {
+            if ((bone.position - previosPosition).sqrMagnitude > sqrDistance)
+            {
+                var temp = bone.position;
+                bone.position = previosPosition;
+                previosPosition = temp;
+            }
+            else
+            {
+                break;
+            }
+        }
+        _transform.position = newPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if(collision.gameObject.tag == "Food")
+        {
+            Destroy(collision.gameObject);
+
+            var bone = Instantiate(BonePrefabe);
+            Tails.Add(bone.transform);
+        } 
+    }
+
+}
